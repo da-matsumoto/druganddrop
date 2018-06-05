9a7292999af64b44963a997682798d7a3d2b15dd
da-matsumoto <da.mats1990@gmail.com>
2018-06-05 19:46:23 +0900

    first commit

---
+++ drug.js
@@ -0,0 +1,55 @@
+(function() {
+    var elements = document.getElementsByClassName("drag-and-drop");
+    var x;
+    var y;
+
+    for(var i = 0; i < elements.length; i++) {
+        elements[i].addEventListener("mousedown", mdown, false);
+        elements[i].addEventListener("touchstart", mdown, false);
+
+        function mdown(e) {
+            this.classList.add("drag");
+
+            if(e.type === "mousedown") {
+                var event = e;
+            } else {
+                var event = e.changedTouches[0];
+            }
+
+            x = event.pageX - this.offsetLeft;
+            y = event.pageY - this.offsetTop;
+
+            document.body.addEventListener("mousemove", mmove, false);
+            document.body.addEventListener("touchmove", mmove, false);
+        }
+
+        function mmove(e) {
+            var drag = document.getElementsByClassName("drag")[0];
+
+            if(e.type === "mousemove") {
+                var event = e;
+            } else {
+                var event = e.changedTouches[0];
+            }
+
+            e.preventDefault();
+
+            drag.style.top = event.pageY - y + "px";
+            drag.style.left = event.pageX -x + "px";
+
+            drag.addEventListener("mouseup", mup, false);
+            document.body.addEventListener("mouseleave", mup, false);
+            drag.addEventListener("touchend", mup, false);
+            document.body.addEventListener("touchleave", mup, false);
+        }
+
+        function mup(e) {
+            var drag = document.getElementsByClassName("drag")[0];
+            document.body.removeEventListener("mouseup", mup, false);
+            document.body.removeEventListener("touchmove", mmove, faluse);
+            drag.removeEventListener("touchend", mup, false);
+
+            frag.classList.remove("drag");
+        }
+    }
+});
